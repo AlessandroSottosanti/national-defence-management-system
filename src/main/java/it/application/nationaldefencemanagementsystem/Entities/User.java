2ad40be230c2ac0a2b7fbc1f082022068f1fe4c1
AlessandroSottosanti <asottosanti2@gmail.com>
2026-07-13 12:10:17 +0200

    feat: Users and Roles entities

---
+++ src/main/java/it/application/nationaldefencemanagementsystem/Entities/User.java
@@ -0,0 +1,33 @@
+package it.application.nationaldefencemanagementsystem.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private boolean enabled = true;
+}
